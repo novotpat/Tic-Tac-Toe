a5566ae5c3cbe59649fcdd9f297fc6870f8863bd
Patrik Novotny <71101156+novotpat@users.noreply.github.com>
2022-04-29 20:34:59 +0200

    Add files via upload

---
+++ game_rules.py
@@ -0,0 +1,8 @@
+Text = ["""GAME RULES:
+Each player can place one mark (or stone)
+per turn on the 3x3 grid. The WINNER is
+whu succeeds in placing three of their
+marks in a:
+* horizontal,
+* vertical or
+* diagonal row"""]
